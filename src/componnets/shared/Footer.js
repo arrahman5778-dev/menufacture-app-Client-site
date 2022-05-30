e61dd70d9b310187856bdb493ebc7600132289a0
arrahman5778 <arrahman5778@gmail.com>
2022-05-30 12:53:15 +0600

    footer title

--- src/componnets/shared/Footer.js
+++ src/componnets/shared/Footer.js
@@ -11,21 +11,21 @@ const Footer = () => {
                     <p>ACME Industries Ltd.<br />Providing reliable tech since 1992</p>
                 </div>
                 <div>
-                    <span className="footer-title">Services</span>
+                    <span className="uppercase font-bold text-gry-400  hover:text-orange-500">Services</span>
                     <a className="link link-hover" >Branding</a>
                     <a className="link link-hover">Design</a>
                     <a className="link link-hover">Marketing</a>
                     <a className="link link-hover">Advertisement</a>
                 </div>
                 <div>
-                    <span className="footer-title">Company</span>
+                    <span className=" uppercase font-bold  hover:text-orange-500">Company</span>
                     <a className="link link-hover">About us</a>
                     <a className="link link-hover">Contact</a>
                     <a className="link link-hover">Jobs</a>
                     <a className="link link-hover">Press kit</a>
                 </div>
                 <div>
-                    <span className="footer-title">Legal</span>
+                    <span className="uppercase font-bold  hover:text-orange-500">Legal</span>
                     <a className="link link-hover">Terms of use</a>
                     <a className="link link-hover">Privacy policy</a>
                     <a className="link link-hover">Cookie policy</a>
